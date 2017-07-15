622d23b38b416d3384abc7ab6a87f43debf9a65d
Daniel Mangowi <mangowidaniel@gmail.com>
2017-07-15 22:10:16 +0200

    Setting UIHint on Product field to display a specific template, changes on date on Products

--- CustomDisplay/Models/Product.cs
+++ CustomDisplay/Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Web;
@@ -12,6 +13,8 @@ public class Product
 
         public string Name { get; set; }
         public decimal Price { get; set; }
+
+        [UIHint("PreOrderOrAvailable")]
         public DateTime ReleaseDate { get; set; }
         public int ProductId { get; set; }
     }
--- CustomDisplay/Models/Products.cs
+++ CustomDisplay/Models/Products.cs
@@ -11,13 +11,13 @@ namespace CustomDisplay.Models
         {
             var products = new List<Product>
                 {
-                   new Product {Name = "Bada", Price = 24.9M, ProductId = 1, ReleaseDate = DateTime.Parse("12/10/1993")},
-                   new Product {Name = "Wada", Price = 2.9M, ProductId = 2, ReleaseDate = DateTime.Parse("11/10/2014")},
-                   new Product {Name = "Tada", Price = 0.9M, ProductId = 3, ReleaseDate = DateTime.Parse("2/10/2000")},
+                   new Product {Name = "Bada", Price = 24.9M, ProductId = 1, ReleaseDate = DateTime.Parse("12/10/2093")},
+                   new Product {Name = "Wada", Price = 2.9M, ProductId = 2, ReleaseDate = DateTime.Parse("11/10/1024")},
+                   new Product {Name = "Tada", Price = 0.9M, ProductId = 3, ReleaseDate = DateTime.Parse("2/10/2050")},
                    new Product {Name = "Vada", Price = 8.9M,ProductId = 4,ReleaseDate = DateTime.Parse("12/10/1993")},
                    new Product {Name = "Fada", Price = 12.9M, ProductId = 5, ReleaseDate = DateTime.Parse("12/10/1993")},
                    new Product {Name = "Sada", Price = 2.9M, ProductId = 6, ReleaseDate = DateTime.Parse("12/10/1993")},
-                   new Product {Name = "Qada", Price = 4.9M, ProductId = 7, ReleaseDate = DateTime.Parse("12/10/1993")}
+                   new Product {Name = "Qada", Price = 4.9M, ProductId = 7, ReleaseDate = DateTime.Parse("12/10/2093")}
 
                 };
           return  products;
